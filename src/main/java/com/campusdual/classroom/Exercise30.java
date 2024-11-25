9f8ac117bb783e5038d58722117c20b8ffdae205
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2024-11-25 09:07:45 +0100

    Ejercicio 30 completado

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -2,7 +2,13 @@ package com.campusdual.classroom;
 
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException {
+
+        if (divisor == 0){
+            throw  new DivisionByZeroException("No se puede dividir por cero");
+        }
+
+        return divisor / dividend;
 
     }
 
